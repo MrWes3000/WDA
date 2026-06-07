e8e9b010c18367deb3d8d0ef99f898fa79bff3f6
HarrisMan <yougotwes@gmail.com>
2026-06-07 07:16:00 -0400

    Update SuperRemover.ps1

--- SuperRemover.ps1
+++ SuperRemover.ps1
@@ -1,18 +1,21 @@
-    Clear-Host
-    Write-Host "===================================="
-    Write-Host "        Main Menu"
-    Write-Host "===================================="
-    Write-Host "1: Disable Sleep Settings"
-    Write-Host "2: Remove Office 365 Bloatware"
-    Write-Host "3: Remove Dell Bloatware 1"
-    Write-Host "4: Remove Dell Bloatware 2"
-    Write-Host "5: Deploy Adobe Reader"
-    Write-Host "6: Deploy Microsoft Remote Desktop Client"
-    Write-Host "7: Deploy Zoom"
-    Write-Host "8: Deploy Chrome"
-    Write-Host "9: Deploy 7-Zip"
-    Write-Host "100: Exit"
-    Write-Host "===================================="
+$menu = @"
+====================================
+        Main Menu
+====================================
+1: Disable Sleep Settings
+2: Remove Office 365 Bloatware
+3: Remove Dell Bloatware 1
+4: Remove Dell Bloatware 2
+5: Deploy Adobe Reader
+6: Deploy Microsoft Remote Desktop Client
+7: Deploy Zoom
+8: Deploy Chrome
+9: Deploy 7-Zip
+100: Exit
+====================================
+"@
+
+Write-Output $menu
  
  function Show-Menu {
     Clear-Host
